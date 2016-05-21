8a839894564c012cc16dee69afd57025cc87d884
SwG Ghosh <codecrafts.cf@icloud.com>
2016-05-21 21:11:17 +0530

    Made navigation bar to stick to top only on scrolling of navbar (index.html), Added script tag js inside body instead of head

--- base.js
+++ base.js
@@ -16,16 +16,20 @@ var changeSlashRepeatedly = function () {
 };
 setInterval(changeSlashRepeatedly, 1000);
 document.onscroll = function () {
-    if(document.body.scrollTop === 0 ) {
-		document.querySelector('.header').style.backgroundColor = '#222';
-        document.querySelector('.navbar').style.position = null;
-        document.querySelector('.navbar').style.top = null;
-        document.querySelector('#flogo').style.display = 'none';
+    if(document.body.scrollTop === 0) {
+        document.querySelector('.header').style.backgroundColor = '#222';
     }
     else {
         document.querySelector('.header').style.backgroundColor = '#456';
+    }
+    if(document.body.scrollTop > parseInt($('.header').height())) {
         document.querySelector('.navbar').style.position = 'fixed';
         document.querySelector('.navbar').style.top = '0';
         document.querySelector('#flogo').style.display = 'block';
+    }
+    else {
+        document.querySelector('.navbar').style.position = null;
+        document.querySelector('.navbar').style.top = null;
+        document.querySelector('#flogo').style.display = 'none';
     }
 };

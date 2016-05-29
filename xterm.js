a0ecf2ef08c880fdedd6ce612af6122dded6ea20
SwG Ghosh <codecrafts.cf@icloud.com>
2016-05-29 15:24:08 +0530

    Included xterm.js code to main function

--- xterm.js
+++ xterm.js
@@ -1,17 +1,17 @@
-var text = "Something amazing will fill up this space soon. We're working hard on it. #comingsoon :)                           ";
-var counter = 0;
-$('#xterm').html('');
-var addText = function () {
-    if(counter === text.length) {
-        $('#xterm').html('');
-        counter = 0;
-    }
-    else {
-        $('#xterm').append(text.charAt(counter));
-        counter = counter + 1;
-    }
-};
 var main = function () {
+    var text = "Something amazing will fill up this space soon. We're working hard on it. #comingsoon :)                           ";
+    var counter = 0;
+    $('#xterm').html('');
+    var addText = function () {
+        if(counter === text.length) {
+            $('#xterm').html('');
+            counter = 0;
+        }
+        else {
+            $('#xterm').append(text.charAt(counter));
+            counter = counter + 1;
+        }
+    };
     setInterval(addText, 150);
 };
 $(document).ready(main);

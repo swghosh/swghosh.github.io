906b4ce739efda13a6be946034c13bb50764a07b
SwG Ghosh <codecrafts.cf@icloud.com>
2016-12-26 01:18:17 +0530

    Preloader functionality modified and added colour

--- preloader.js
+++ preloader.js
@@ -1,18 +1,27 @@
 /* Pre Loader working */
 function endpreloader(element) {
-    element.style.display = 'none';
+    element.className = "no preloader";
 }
+
+// displays the preloader to off when the window has finished loading
+
 window.addEventListener("load", pageFullyLoaded, false);
+
 function pageFullyLoaded(e) {
-    // do something again
-    endpreloader(document.querySelector('li#preloader'));
+    endpreloader(document.querySelector('div.preloader'));
 }
+
 var rotateLoader = function() {
-    if (document.querySelector('section.loader').style.transform === 'rotate(360deg)') {
-        document.querySelector('section.loader').style.transform = 'rotate(0deg)';
-    }
-    else if (document.querySelector('section.loader').style.transform === 'rotate(0deg)') {
-        document.querySelector('section.loader').style.transform = 'rotate(360deg)';
-    }
+//    if (document.querySelector('section.loader').style.transform === 'rotate(360deg)') {
+//        document.querySelector('section.loader').style.transform = 'rotate(0deg)';
+//    }
+//    else if (document.querySelector('section.loader').style.transform === 'rotate(0deg)') {
+//        document.querySelector('section.loader').style.transform = 'rotate(360deg)';
+//    }
+    var a = document.querySelector('section.loader').style.transform;
+    a = a.split("(")[1].split("deg")[0];
+    var i = parseInt(a) + 360;
+    document.querySelector('section.loader').style.transform = 'rotate(' + i.toString() + 'deg)';
 };
+
 setInterval(rotateLoader, 2000);
